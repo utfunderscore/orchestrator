e57524c7a9540d3d12910c2dfec48bb210d9f11d
Connor <concon477@virginmedia.com>
2024-07-13 13:41:44 +0100

    fix: Remove packet system

--- Shared/src/main/kotlin/org/readutf/orchestrator/shared/packet/serializer/PacketSerializer.kt
+++
@@ -1,9 +0,0 @@
-package org.readutf.orchestrator.shared.packet.serializer
-
-import org.readutf.orchestrator.shared.packet.Packet
-
-interface PacketSerializer {
-    fun serialize(packet: Packet): ByteArray
-
-    fun deserialize(bytes: ByteArray): Packet
-}
