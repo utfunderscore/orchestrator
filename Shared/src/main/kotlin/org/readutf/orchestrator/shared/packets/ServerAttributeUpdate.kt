ddfac44c2cd72bced5f1ea011a9fbb9680fbb5ea
Connor <concon477@virginmedia.com>
2024-08-25 14:27:00 +0100

    fix: Server data now retains types for attributes

--- Shared/src/main/kotlin/org/readutf/orchestrator/shared/packets/ServerAttributeUpdate.kt
+++ Shared/src/main/kotlin/org/readutf/orchestrator/shared/packets/ServerAttributeUpdate.kt
@@ -1,10 +1,11 @@
 package org.readutf.orchestrator.shared.packets
 
 import org.readutf.hermes.Packet
+import org.readutf.orchestrator.shared.utils.TypedObject
 import java.util.*
 
 data class ServerAttributeUpdate(
     val serverId: UUID,
     val attributeName: String,
-    val attribute: Any,
+    val attribute: TypedObject,
 ) : Packet()
