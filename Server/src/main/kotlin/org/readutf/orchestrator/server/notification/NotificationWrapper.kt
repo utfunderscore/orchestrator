1cdf1b7180a33a482a999698755c35fe0eca025f
utfunderscore <concon477@virginmedia.com>
2024-10-01 21:49:17 +0100

    fix: Move notification wrapper to shared module, and convert to data class

--- Server/src/main/kotlin/org/readutf/orchestrator/server/notification/NotificationWrapper.kt
+++
@@ -1,9 +0,0 @@
-package org.readutf.orchestrator.server.notification
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo
-import org.readutf.orchestrator.shared.notification.Notification
-
-class NotificationWrapper(
-    @field:JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.CLASS)
-    val notification: Notification,
-)
