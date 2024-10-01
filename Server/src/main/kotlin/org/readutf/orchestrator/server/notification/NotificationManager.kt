0c412f8a5a83f912c41f100bc505554822dcdc74
utfunderscore <concon477@virginmedia.com>
2024-10-01 21:51:48 +0100

    fix: Enable heartbeat notifications for web sockets

--- Server/src/main/kotlin/org/readutf/orchestrator/server/notification/NotificationManager.kt
+++ Server/src/main/kotlin/org/readutf/orchestrator/server/notification/NotificationManager.kt
@@ -3,10 +3,18 @@ package org.readutf.orchestrator.server.notification
 import io.javalin.websocket.WsConfig
 import io.javalin.websocket.WsContext
 import org.readutf.orchestrator.shared.notification.Notification
+import org.readutf.orchestrator.shared.notification.NotificationWrapper
+import java.util.concurrent.Executors
 import java.util.function.Consumer
 
 object NotificationManager {
     val activeListeners = mutableListOf<WsContext>()
+
+    init {
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate({
+            activeListeners.forEach { it.sendPing() }
+        }, 0, 1000, java.util.concurrent.TimeUnit.MILLISECONDS)
+    }
 
     fun notifyAll(notification: Notification) {
         activeListeners.forEach {
