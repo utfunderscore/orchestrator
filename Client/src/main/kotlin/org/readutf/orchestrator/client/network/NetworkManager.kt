e97e31626a9ddc74e00d20044306637e9989acce
utfunderscore <concon477@virginmedia.com>
2024-10-01 21:51:08 +0100

    fix: Update to latest hermes version and use kryo resource pooling

--- Client/src/main/kotlin/org/readutf/orchestrator/client/network/NetworkManager.kt
+++ Client/src/main/kotlin/org/readutf/orchestrator/client/network/NetworkManager.kt
@@ -1,44 +1,70 @@
 package org.readutf.orchestrator.client.network
 
+import com.esotericsoftware.kryo.kryo5.Kryo
+import com.esotericsoftware.kryo.kryo5.util.Pool
 import org.readutf.hermes.Packet
 import org.readutf.hermes.PacketManager
 import org.readutf.hermes.channel.ChannelClosePacket
+import org.readutf.hermes.channel.ChannelOpenPacket
 import org.readutf.hermes.channel.HermesChannel
 import org.readutf.hermes.listeners.Listener
+import org.readutf.hermes.platform.netty.NettyClientPlatform
 import org.readutf.hermes.platform.netty.nettyClient
 import org.readutf.hermes.serializer.KryoPacketSerializer
-import org.readutf.orchestrator.client.OrchestratorClient
 import org.readutf.orchestrator.shared.kryo.KryoCreator
 import java.net.SocketException
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 class NetworkManager(
-    val client: OrchestratorClient,
+    private val orchestratorHost: String,
+    private val orchestratorPort: Int,
+    private val kryo: Kryo,
+    val onDisconnect: () -> Unit,
 ) {
     val packetManager = createPacketManager()
 
-    private fun createPacketManager() =
-        PacketManager
-            .nettyClient(
-                client.orchestratorHost,
-                client.orchestratorPort,
-                KryoPacketSerializer(KryoCreator.build()),
-            ).editListeners {
-                it.registerListener(
-                    ChannelClosePacket::class.java,
-                    object : Listener {
-                        override fun acceptPacket(
-                            hermesChannel: HermesChannel,
-                            packet: Packet,
-                        ) {
-                            client.onDisconnect()
-                        }
-                    },
-                )
-            }.exception {
-                println("error")
-            }.exception(SocketException::class.java) {
-                client.onDisconnect()
-            }.start()
+    private fun createPacketManager(): PacketManager<NettyClientPlatform> {
+        val startAwait = CompletableFuture<Unit>()
+        val packetManager =
+            PacketManager
+                .nettyClient(
+                    hostName = orchestratorHost,
+                    port = orchestratorPort,
+                    serializer =
+                        KryoPacketSerializer(
+                            object : Pool<Kryo>(true, false, 16) {
+                                override fun create(): Kryo = KryoCreator.build()
+                            },
+                        ),
+                    executorService = Executors.newSingleThreadExecutor(),
+                ).editListeners {
+                    it.registerListener<ChannelOpenPacket<*>> {
+                        println("Connected to orchestrator")
+                        startAwait.complete(null)
+                    }
+                    it.registerListener(
+                        ChannelClosePacket::class.java,
+                        object : Listener {
+                            override fun acceptPacket(
+                                hermesChannel: HermesChannel,
+                                packet: Packet,
+                            ) {
+                                onDisconnect()
+                            }
+                        },
+                    )
+                }.exception {
+                    it.printStackTrace()
+                }.exception(SocketException::class.java) {
+                    onDisconnect()
+                }.start()
+
+        println("awaiting connection")
+        startAwait.join()
+
+        return packetManager
+    }
 
     fun shutdown() {
         packetManager.stop()
@@ -51,4 +77,6 @@ class NetworkManager(
             e.printStackTrace()
         }
     }
+
+    inline fun <reified T> sendPacketWithResponse(packet: Packet): CompletableFuture<T> = packetManager.sendPacket<T>(packet)
 }
