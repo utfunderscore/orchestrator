7c27be0e8b7f0cb8c0d91c599dc81a030374b78e
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-02-27 20:23:57 +0000

    refactor: Remove redundant enableGameServer function overload in ClientManager

--- client/src/main/kotlin/org/readutf/orchestrator/client/client/game/GameExtensions.kt
+++ client/src/main/kotlin/org/readutf/orchestrator/client/client/game/GameExtensions.kt
@@ -1,25 +1,9 @@
 package org.readutf.orchestrator.client.client.game
 
 import org.readutf.orchestrator.client.client.ClientManager
-import org.readutf.orchestrator.common.game.Game
 import org.readutf.orchestrator.common.game.GameFinderType
 import org.readutf.orchestrator.common.game.GameServerSettings
-import java.util.UUID
 import java.util.concurrent.TimeUnit
-
-public fun ClientManager.enableGameServer(
-    supportedGames: List<String>,
-    finderTypes: List<GameFinderType>,
-    activeGamesProvider: () -> List<Game>,
-    gameRequestHandler: () -> UUID,
-) {
-    enableGameServer(
-        supportedGames,
-        finderTypes,
-        ActiveGamesProvider(activeGamesProvider),
-        GameRequestHandler(gameRequestHandler),
-    )
-}
 
 public fun ClientManager.enableGameServer(
     supportedGames: List<String>,
